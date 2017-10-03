3afbbef20fd0838e3d7cbb0bfe05964ebfbbf014
Chamalows <32468254+Chamalows@users.noreply.github.com>
2017-10-03 08:41:37 +0200

    Update TP2.py

--- TP2.py
+++ TP2.py
@@ -22,3 +22,12 @@ B2 = -eye(Ns)
 B3 = zeros((Ns, Ns))
 
 A[[0,3],:]]
+
+#Autre methode de creation de la matrice A
+
+A = -1*(diag(ones(Ns*Ns-3),3) + diag(ones(Ns*Ns-3),-3))
+
+B = 4*eye(Ns) -1*(diag(ones(Ns-1),1) + diag(ones(Ns-1),-1))
+
+for i in arange(0,Ns):
+  A[3*i:3*i+3,3*i:3*i+3] = B
